d0b942ce181f82f09c2e6f9ad26b2f12590d0914
Martin Olmr <info@martinolmr.cz>
2021-02-24 11:38:02 +0100

    add default text email template

---
+++ install/email/TextMail.php
@@ -0,0 +1,16 @@
+<?php
+
+declare(strict_types=1);
+
+namespace MatiCore\Email;
+
+use MatiCore\Email\Email\BaseEmail;
+
+/**
+ * Class TextMail
+ * @package MatiCore\Email
+ */
+class TextMail extends BaseEmail
+{
+
+}
